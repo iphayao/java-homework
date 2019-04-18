8d23c50afb00eced333e5dbc6b931428ec58c82b
iphayao <phayaob@gmail.com>
2019-04-18 11:53:06 +0700

    Add implement of step 3, sync movies data to DB

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,17 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.MovieMapper;
+import com.wongnai.interview.movie.external.MovieData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +25,10 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		List<Movie> movies = movieDataService.fetchAll().stream()
+				.map(MovieMapper.INSTANCE::movieDataToMovie)
+				.collect(Collectors.toList());
+
+		movieRepository.saveAll(movies);
 	}
 }
